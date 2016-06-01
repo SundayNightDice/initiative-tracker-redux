21d202c1232361aa3cf9a74bd2e7cadc2268aeba
Sam Hogarth <dev@samhogy.co.uk>
2016-06-01 15:21:18 +0100

    Drops unused props from encounter

--- src/components/encounter.js
+++ src/components/encounter.js
@@ -14,17 +14,11 @@ const Encounter = (props) => (
       turn={props.turn}
       damageTargets={props.damageTargets}
       healingTargets={props.healingTargets}
-      onDamageTargetSelected={props.onDamageTargetSelected}
-      onDamageChange={props.onDamageChange}
-      onToggleApplyCondition={props.onToggleApplyCondition}
       onToggleCondition={props.onToggleCondition}
       onApplyDamage={props.onApplyDamage}
-      onHealTargetSelected={props.onHealTargetSelected}
-      onHealChange={props.onHealChange}
       onApplyHealing={props.onApplyHealing}
       onDeathSave={props.onDeathSave}
       onDeathFail={props.onDeathFail}
-      onCriticalDamage={props.onCriticalDamage}
       onCriticalSave={props.onCriticalSave}
       onEndTurn={props.onEndTurn} />
   </div>
