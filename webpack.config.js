626a596adb9f6a4b9b61c67514babd707fd9cca4
Sam Hogarth <dev@samhogy.co.uk>
2016-09-30 21:28:50 +0100

    Adding file loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,14 @@ module.exports = {
       {
         test: /\.less$/,
         loader: 'style!css!less?noIeCompat'
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url?limit=25000'
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.less']
+    extensions: ['', '.js', '.less', '.jpg', '.png']
   }
 };
