114a06d28545a27773ab296d381dc2e7dc6085ed
Sam Hogarth <dev@samhogy.co.uk>
2016-11-05 14:34:33 +0000

    Styling the players list and form

--- src/components/playersList.js
+++ src/components/playersList.js
@@ -2,14 +2,19 @@ import React from 'react';
 import AddPlayer from './addPlayer';
 import StatBlock from './statblock';
 
-const Player = (props) => (
-  <li>
-    <span>{props.player.name}</span>
-    <span className="level">Level {props.player.level} {props.player.class}</span>
-    <span className="hp">({props.player.hp + ' / ' + props.player.maxHp + ' HP'})</span>
-    <StatBlock attributes={props.player.attributes} modifiers={props.player.modifiers} />
-  </li>
-);
+const Player = (props) => {
+  const details = `Level ${props.player.level} ${props.player.class}`;
+  const hp = `(${props.player.hp}/${props.player.maxHp} HP)`;
+
+  return (
+    <li className="player">
+      <span className="name">{props.player.name}</span>
+      <span className="details">{details}</span>
+      <span className="hp">{hp}</span>
+      <StatBlock attributes={props.player.attributes} modifiers={props.player.modifiers} />
+    </li>
+  );
+};
 
 const PlayersList = (props) => (
   <div className="players">
