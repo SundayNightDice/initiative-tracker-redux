a431914c720a307a4ee339500e6c917cdd9b653e
Sam Hogarth <dev@samhogy.co.uk>
2016-11-12 14:49:00 +0000

    Resets turn forms on end of turn

--- client/actions/endTurn.js
+++ client/actions/endTurn.js
@@ -1,6 +1,12 @@
-export default function endTurn(encounterId) {
-  return {
-    type: 'END_TURN',
-    encounterId: encounterId
-  };
+import { reset } from 'redux-form';
+
+export default (encounterId) => {
+  return (dispatch, getState) => {
+    dispatch({
+      type: 'END_TURN',
+      encounterId: encounterId
+    });
+    dispatch(reset('turnDamage'));
+    dispatch(reset('turnHealing'));
+  }
 }
